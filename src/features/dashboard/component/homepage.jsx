4f7ee4844e30412d584bf48a7e3744b7931b58da
ForamGhoniya <foramghoniya2002@gmail.com>
2023-06-16 14:23:22 +0530

    homepage design

--- src/features/dashboard/component/homepage.jsx
+++ src/features/dashboard/component/homepage.jsx
@@ -1,7 +1,8 @@
 import '../../dashboard/style/homepage.scss';
 import jsonData from '../json/fitness';
 import { useNavigate } from "react-router-dom";
-
+import UserLogo from "../../../assets/images/avatar.png";
+import running from "../../../assets/images/run.jpg";
 
 const  HomePage= () => {
   const navigate = useNavigate();
@@ -11,29 +12,37 @@ const  HomePage= () => {
       <>
     <div className="dashboard-wrapper" >
         <div className="fitness-activity-dash">
-            <div className="fit-activity">
+
+        <div className="fit-activity">
+          <div className='userDataS'>
+          <img src={UserLogo} alt="User logo" className='userImg' />
+          <p className='userName'>{jsonData.user.name}</p>
+          </div>
+
+          
               <p>Track Your Daily Activity</p>
-              <div class="activity-img">
+              <div className="activity-img">
               </div>
               </div>
               
-              <div class="list-activity">
-                <button class="activity-name" onClick={() => {navigate('/activityInfo')}}>
-                <i class="fa-solid fa-person-running"></i> {jsonData.activities[0].name}
+              <div className="list-activity">
+                <button className="activity-name running" onClick={() => {navigate('/activityInfo')}}>
+                {/* <img src={running} className='runImg' /> */}
+                {jsonData.activities[0].name}
                 </button>
-                <button class="activity-name" onClick={() => {navigate('/activityInfo')}}>
-                <i class="fa-solid fa-person-biking"></i> {jsonData.activities[1].name}
+                <button className="activity-name" onClick={() => {navigate('/activityInfo')}}>
+                {jsonData.activities[1].name}
                 </button>
-                <button class="activity-name" onClick={() => {navigate('/activityInfo')}}>
-                <i class="fa-solid fa-person-running-fast"></i> {jsonData.activities[2].name}
+                <button className="activity-name" onClick={() => {navigate('/activityInfo')}}>
+                {jsonData.activities[2].name}
                 </button>
-                <button class="activity-name" onClick={() => {navigate('/activityInfo')}}>
-                <i class="fa-solid fa-person-swimming"></i> {jsonData.activities[3].name}
+                <button className="activity-name" onClick={() => {navigate('/activityInfo')}}>
+                {jsonData.activities[3].name}
                 </button>
-                <button class="activity-name" onClick={() => {navigate('/activityInfo')}}>
-                <i class="fa-solid fa-person-hiking"></i> {jsonData.activities[4].name}
+                <button className="activity-name" onClick={() => {navigate('/activityInfo')}}>
+                {jsonData.activities[4].name}
                 </button>
-                <button class="activity-name" onClick={() => {navigate('/activityInfo')}}>
+                <button className="activity-name" onClick={() => {navigate('/activityInfo')}}>
               {jsonData.activities[5].name}
                 </button>
               </div>
