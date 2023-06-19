f07fbbdbae0ef58b8ea736807190baeca516e56e
ForamGhoniya <foramghoniya2002@gmail.com>
2023-06-19 14:09:05 +0530

    remove unused code

--- src/features/lendingPage/component/lending.jsx
+++ src/features/lendingPage/component/lending.jsx
@@ -8,17 +8,17 @@ const Lending = () => {
 
     return (
       <>
-      <div class="dashboard-wrapper" id="lending-page">
-        <div class="first-page">
+      <div className="dashboard-wrapper" id="lending-page">
+        <div className="first-page">
         </div>
-        <div class="text-wrapper">
+        <div className="text-wrapper">
           <h2>Welcome To The Future of fitness</h2>
           <p>
           Best workouts for you
           </p>
-          <p class="description">You will have everything you need to reach your personal fitness goals - for free!</p>
-          <div class="button-wrapper">
-            <button class="start-button" onClick={()=> {navigate('/home')}}>Get Started <i class="fa-solid fa-person-running"></i></button>
+          <p className="description">You will have everything you need to reach your personal fitness goals - for free!</p>
+          <div className="button-wrapper">
+            <button className="start-button" onClick={()=> {navigate('/home')}}>Get Started <i className="fa-solid fa-person-running"></i></button>
           </div>
           </div>
       </div>
